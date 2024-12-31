bde8cfcff2e3f90e4b1d1b058ce64ff34eb4c4e7
jay <jianyoang@gmail.com>
2024-12-31 17:50:24 +0800

    [main] Simplify responsibility of the script for now to avoid rewriting git history

--- Sources/PreFixIt.swift
+++ Sources/PreFixIt.swift
@@ -4,10 +4,7 @@ import ArgumentParser
 @main
 struct PreFixIt: ParsableCommand {
 
-    @Flag(name: [.customLong("last")], help: "Apply only to the last commit")
-    var shouldOnlyApplyToLastCommit: Bool = false
-
-    @Flag(name: .long, help: "Display logs for debugging purposes")
+    @Flag(name: .short, help: "Display logs for debugging purposes")
     var verbose: Bool = false
     
     func run() throws {
@@ -16,23 +13,13 @@ struct PreFixIt: ParsableCommand {
             return
         }
         
-        if shouldOnlyApplyToLastCommit {
-            guard let lastCommitMessage = getLastCommitMessage(), !lastCommitMessage.isEmpty else {
-                print("PreFixIt needs a commit message. Currently commit message seems to be nil or empty")
-                return
-            }
-            printProgressIfNeeded("PreFixIt updating last commit message")
-            updateCommitMessage(with: branchName, existingMessage: lastCommitMessage)
-        } else {
-            let allCommitMessages = getAllCommitMessages()
-            guard !allCommitMessages.isEmpty else {
-                print("PreFixIt needs a commit message. Currently commit message seems to be nil or empty")
-                return
-            }
-        
-            printProgressIfNeeded("PreFixIt updating commit messages")
-            updateAllCommitMessages(with: branchName, commitMessages: allCommitMessages)
+        guard let lastCommitMessage = getLastCommitMessage(), !lastCommitMessage.isEmpty else {
+            print("PreFixIt needs a commit message. Currently commit message seems to be nil or empty")
+            return
         }
+        
+        printProgressIfNeeded("PreFixIt updating last commit message")
+        updateCommitMessage(with: branchName, existingMessage: lastCommitMessage)
     }
 }
 
@@ -48,39 +35,6 @@ private extension PreFixIt {
     func getLastCommitMessage() -> String? {
         printProgressIfNeeded("Getting last commit message")
         return runShell("git log -1 --pretty=%B")
-    }
-    
-    func getAllCommitMessages() -> [String] {
-        printProgressIfNeeded("Getting all commit messages of current branch")
-        
-        guard let commitMessage = runShell("git log --pretty=%B") else {
-            print("Failed to retrieve commit messages")
-            return []
-        }
-        
-        return commitMessage.split(separator: "\n").map { String($0) }
-    }
-    
-    func updateAllCommitMessages(with branchName: String, commitMessages: [String]) {
-        printProgressIfNeeded("Starting rebase interactive mode")
-        
-        let rebaseCommand = "git rebase -i --root"
-        guard runShell(rebaseCommand) != nil else {
-            print("Hmmm....PreFixIt failed to rebase. Please ensure Git is up and running appropriately")
-            return
-        }
-        
-        for commitMessage in commitMessages {
-            updateCommitMessage(with: branchName, existingMessage: commitMessage)
-        }
-        
-        let rebaseContinueCommand = "git rebase --continue"
-        guard runShell(rebaseContinueCommand) != nil else {
-            printProgressIfNeeded("PreFixIt failed to complete rebase operation. Sorry, please help resolve the conflicts and manually")
-            return
-        }
-        
-        printProgressIfNeeded("Successfully rebased commits with branch name as its prefix")
     }
     
     func updateCommitMessage(with branchName: String, existingMessage: String) {
